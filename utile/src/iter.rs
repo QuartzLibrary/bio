51007d2a0dccef34e11128718ed6407149c20524
QuartzLibrary <81446760+QuartzLibrary@users.noreply.github.com>
2025-05-04 19:55:08 +0100

    Add `StagedSortedIterator`

--- utile/src/iter.rs
+++ utile/src/iter.rs
@@ -1,4 +1,27 @@
-use std::ops::Range;
+use std::{cmp::Ordering, collections::VecDeque, iter::Peekable, ops::Range};
+
+pub trait IteratorExt: Iterator + Sized {
+    fn spliced(
+        self,
+        range: Range<usize>,
+        insert: impl Iterator<Item = Self::Item>,
+    ) -> SplicedIterator<Self, impl Iterator<Item = Self::Item>>
+    where
+        Self: ExactSizeIterator,
+    {
+        SplicedIterator::new(self, range, insert)
+    }
+
+    /// Sort an iterator already sorted by the first stage by the second stage.
+    fn staged_sorted_by<S1, S2>(self, s1: S1, s2: S2) -> StagedSortedIterator<Self, S1, S2>
+    where
+        S1: FnMut(&Self::Item, &Self::Item) -> Ordering,
+        S2: FnMut(&Self::Item, &Self::Item) -> Ordering,
+    {
+        StagedSortedIterator::new(self, s1, s2)
+    }
+}
+impl<T: Iterator> IteratorExt for T {}
 
 pub struct SplicedIterator<Base, Insert> {
     inner: Option<SplicedIteratorInner<Base, Insert>>,
@@ -109,6 +132,69 @@ impl<Base: ExactSizeIterator, Insert: Iterator<Item = Base::Item> + ExactSizeIte
             } => base.len() - remove + insert.len(),
             SplicedIteratorInner::Insert { insert, base } => insert.len() + base.len(),
             SplicedIteratorInner::End { base } => base.len(),
+        }
+    }
+}
+
+pub struct StagedSortedIterator<Base, S1, S2>
+where
+    Base: Iterator,
+{
+    base: Peekable<Base>,
+    s1: S1,
+    s2: S2,
+    stage: VecDeque<Base::Item>,
+}
+impl<Base, S1, S2> StagedSortedIterator<Base, S1, S2>
+where
+    Base: Iterator,
+    S1: FnMut(&Base::Item, &Base::Item) -> Ordering,
+    S2: FnMut(&Base::Item, &Base::Item) -> Ordering,
+{
+    pub fn new(base: Base, s1: S1, s2: S2) -> Self {
+        Self {
+            base: base.peekable(),
+            s1,
+            s2,
+            stage: VecDeque::new(),
+        }
+    }
+}
+impl<Base, S1, S2> Iterator for StagedSortedIterator<Base, S1, S2>
+where
+    Base: Iterator,
+    S1: FnMut(&Base::Item, &Base::Item) -> Ordering,
+    S2: FnMut(&Base::Item, &Base::Item) -> Ordering,
+    Base::Item: std::fmt::Debug,
+{
+    type Item = Base::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(item) = self.stage.pop_front() {
+            return Some(item);
+        }
+
+        self.stage.push_back(self.base.next()?);
+        let mut multiple = false;
+        while let Some(item) = self.base.peek() {
+            match (self.s1)(&self.stage[0], item) {
+                Ordering::Less => break,
+                Ordering::Equal => {
+                    multiple = true;
+                    self.stage.push_back(self.base.next()?);
+                }
+                Ordering::Greater => unreachable!(
+                    "Iterator is not sorted by first stage {:?}, {:?}",
+                    self.stage[0], item
+                ),
+            }
+        }
+
+        if multiple {
+            self.stage.make_contiguous().sort_by(&mut self.s2);
+            self.next()
+        } else {
+            Some(self.stage.pop_front().unwrap())
         }
     }
 }
