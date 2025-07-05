8dda8056e9ed381bcf9cec13f79ef8efeb2190f8
QuartzLibrary <81446760+QuartzLibrary@users.noreply.github.com>
2025-07-05 14:52:01 +0100

    Improve `Counting{BTree,Hash}Set`

--- utile/src/collections.rs
+++ utile/src/collections.rs
@@ -1,8 +1,10 @@
 pub mod counting_set {
     use serde::{Deserialize, Serialize};
     use std::{
+        cmp::Ordering,
         collections::{BTreeMap, HashMap},
         hash::Hash,
+        ops::{Add, AddAssign, Sub, SubAssign},
     };
 
     #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -34,24 +36,53 @@ pub mod counting_set {
             *self.0.entry(value).or_default() += 1;
         }
         pub fn increment_by(&mut self, value: T, count: usize) {
-            *self.0.entry(value).or_insert(0) += count;
+            *self.0.entry(value).or_default() += count;
         }
+
         pub fn decrement(&mut self, value: &T) {
             assert!(self.try_decrement(value));
         }
+        pub fn decrement_by(&mut self, value: &T, count: usize) {
+            for _ in 0..count {
+                self.decrement(value);
+            }
+        }
+
+        /// Decrements the count by one, saturating at zero.
+        /// Returns false if there were not enough items to decrement.
         pub fn try_decrement(&mut self, value: &T) -> bool {
+            self.try_decrement_by(value, 1)
+        }
+
+        /// Decrements the count by the given amount, saturating at zero.
+        /// Returns false if there were not enough items to decrement.
+        pub fn try_decrement_by(&mut self, value: &T, count: usize) -> bool {
+            if count == 0 {
+                return true;
+            }
+
             let Some(c) = self.0.get_mut(value) else {
                 return false;
             };
 
-            *c -= 1;
+            assert_ne!(*c, 0);
 
-            if *c == 0 {
-                self.0.remove(value);
+            match Ord::cmp(c, &count) {
+                Ordering::Less => {
+                    self.0.remove(value);
+                    false
+                }
+                Ordering::Equal => {
+                    self.0.remove(value);
+                    true
+                }
+                Ordering::Greater => {
+                    *c -= count;
+                    true
+                }
             }
-
-            true
         }
+
         pub fn remove(&mut self, value: &T) -> Option<usize> {
             self.0.remove(value)
         }
@@ -117,8 +148,73 @@ pub mod counting_set {
             self.0.iter()
         }
     }
+    impl<T> Add for CountingHashSet<T>
+    where
+        T: Hash + Eq,
+    {
+        type Output = Self;
 
-    #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+        fn add(self, other: Self) -> Self {
+            let mut new = self;
+            for (key, value) in other.0 {
+                new.increment_by(key, value);
+            }
+            new
+        }
+    }
+    impl<T> AddAssign for CountingHashSet<T>
+    where
+        T: Hash + Eq,
+    {
+        fn add_assign(&mut self, other: Self) {
+            for (key, value) in other.0 {
+                self.increment_by(key, value);
+            }
+        }
+    }
+    impl<T> Sub for CountingHashSet<T>
+    where
+        T: Hash + Eq,
+    {
+        type Output = Self;
+
+        fn sub(self, other: Self) -> Self {
+            let mut new = self;
+            for (key, value) in other.0 {
+                new.decrement_by(&key, value);
+            }
+            new
+        }
+    }
+    impl<T> SubAssign for CountingHashSet<T>
+    where
+        T: Hash + Eq,
+    {
+        fn sub_assign(&mut self, other: Self) {
+            for (key, value) in other.0 {
+                self.decrement_by(&key, value);
+            }
+        }
+    }
+    impl<T> From<Vec<(T, usize)>> for CountingHashSet<T>
+    where
+        T: Hash + Eq,
+    {
+        fn from(value: Vec<(T, usize)>) -> Self {
+            value.into_iter().collect()
+        }
+    }
+    impl<T> From<CountingHashSet<T>> for Vec<(T, usize)>
+    where
+        T: Hash + Eq + Clone,
+    {
+        fn from(value: CountingHashSet<T>) -> Self {
+            value.into_iter().collect()
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    #[derive(Serialize, Deserialize)]
     #[serde(transparent)]
     #[serde(bound(deserialize = "T: Deserialize<'de> + Ord"))]
     pub struct CountingBTreeSet<T>(BTreeMap<T, usize>);
@@ -151,27 +247,56 @@ pub mod counting_set {
         }
 
         pub fn increment(&mut self, value: T) {
-            *self.0.entry(value).or_insert(0) += 1;
+            *self.0.entry(value).or_default() += 1;
         }
         pub fn increment_by(&mut self, value: T, count: usize) {
-            *self.0.entry(value).or_insert(0) += count;
+            *self.0.entry(value).or_default() += count;
         }
+
         pub fn decrement(&mut self, value: &T) {
             assert!(self.try_decrement(value));
         }
+        pub fn decrement_by(&mut self, value: &T, count: usize) {
+            for _ in 0..count {
+                self.decrement(value);
+            }
+        }
+
+        /// Decrements the count by one, saturating at zero.
+        /// Returns false if there were not enough items to decrement.
         pub fn try_decrement(&mut self, value: &T) -> bool {
+            self.try_decrement_by(value, 1)
+        }
+
+        /// Decrements the count by the given amount, saturating at zero.
+        /// Returns false if there were not enough items to decrement.
+        pub fn try_decrement_by(&mut self, value: &T, count: usize) -> bool {
+            if count == 0 {
+                return true;
+            }
+
             let Some(c) = self.0.get_mut(value) else {
                 return false;
             };
 
-            *c -= 1;
+            assert_ne!(*c, 0);
 
-            if *c == 0 {
-                self.0.remove(value);
+            match Ord::cmp(c, &count) {
+                Ordering::Less => {
+                    self.0.remove(value);
+                    false
+                }
+                Ordering::Equal => {
+                    self.0.remove(value);
+                    true
+                }
+                Ordering::Greater => {
+                    *c -= count;
+                    true
+                }
             }
-
-            true
         }
+
         pub fn remove(&mut self, value: &T) -> Option<usize> {
             self.0.remove(value)
         }
@@ -235,6 +360,70 @@ pub mod counting_set {
 
         fn into_iter(self) -> Self::IntoIter {
             self.0.iter()
+        }
+    }
+    impl<T> Add for CountingBTreeSet<T>
+    where
+        T: Ord,
+    {
+        type Output = Self;
+
+        fn add(self, other: Self) -> Self {
+            let mut new = self;
+            for (key, value) in other.0 {
+                new.increment_by(key, value);
+            }
+            new
+        }
+    }
+    impl<T> AddAssign for CountingBTreeSet<T>
+    where
+        T: Ord,
+    {
+        fn add_assign(&mut self, other: Self) {
+            for (key, value) in other.0 {
+                self.increment_by(key, value);
+            }
+        }
+    }
+    impl<T> Sub for CountingBTreeSet<T>
+    where
+        T: Ord,
+    {
+        type Output = Self;
+
+        fn sub(self, other: Self) -> Self {
+            let mut new = self;
+            for (key, value) in other.0 {
+                new.decrement_by(&key, value);
+            }
+            new
+        }
+    }
+    impl<T> SubAssign for CountingBTreeSet<T>
+    where
+        T: Ord,
+    {
+        fn sub_assign(&mut self, other: Self) {
+            for (key, value) in other.0 {
+                self.decrement_by(&key, value);
+            }
+        }
+    }
+    impl<T> From<Vec<(T, usize)>> for CountingBTreeSet<T>
+    where
+        T: Ord,
+    {
+        fn from(value: Vec<(T, usize)>) -> Self {
+            value.into_iter().collect()
+        }
+    }
+    impl<T> From<CountingBTreeSet<T>> for Vec<(T, usize)>
+    where
+        T: Ord + Clone,
+    {
+        fn from(value: CountingBTreeSet<T>) -> Self {
+            value.into_iter().collect()
         }
     }
 }
